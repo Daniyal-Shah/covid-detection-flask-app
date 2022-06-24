30081b861420643dc1e9ff234976a7b389a89afe
Daniyal-Shah <daniyalshah7862018@gmail.com>
2022-06-24 20:23:52 +0500

    UI for flask app

--- application.py
+++ application.py
@@ -14,13 +14,9 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text=request.form.values())
 
 if __name__ == "__main__":
     application.run(debug=True)
